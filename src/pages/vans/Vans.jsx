c37d8382f3809be8560770712b8ac9722d2bd8ee
Udeme98 <100492222+Udeme98@users.noreply.github.com>
2023-09-30 13:16:34 +0100

    Added useRouteError() hook to display error caught

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { getVans } from "../../../api";
 
